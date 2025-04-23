54b23fce4ff599d69eb23677a28f4a08c4c8c88d
swu1747 <swu1747@gmail.com>
2025-04-23 16:14:24 -0400

    save

---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const App = () => {
+    return (<div>new app</div>)
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello This is a Javascript project.");
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import App from "./app.jsx";
+const root = createRoot(document.getElementById('root'))
+
+root.render(<App />)
